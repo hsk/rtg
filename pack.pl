5cf4d5ad48d8ff1a9b9db3297368d6660ced325b
hsk <sakurai777@gmail.com>
2018-07-16 04:49:10 +0900

    change pack.pl

--- pack.pl
+++ pack.pl
@@ -1,5 +1,6 @@
 name(rtg).
 version('0.0.4').
 title('Regular Tree Grammer validator').
-author('Hiroshi Sakurai', 'http://github.com/hsk').
+author('Hiroshi Sakurai', 'sakurai777@gmail.com').
 home('https://github.com/hsk/rtg').
+download('https://github.com/hsk/rtg.git').
